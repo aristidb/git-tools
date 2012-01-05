9e692cdf077f8033eeff5147dcd219bac55ae59f
Aristid Breitkreuz <aristidb@googlemail.com>
2012-01-05 01:08:09 +0100

    OK, seems to work

--- git-backup.sh
+++ git-backup.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
 BACKUP_BRANCH=refs/heads/backup
-export GIT_INDEX_FILE=$(mktemp /tmp/git-idx-XXXX)
+BACKUP_MESSAGE=Backup
+TMPDIR=$(mktemp -d /tmp/git-backup-XXXX)
+export GIT_INDEX_FILE=$TMPDIR/index
 
-LAST_COMMIT=$(git rev-parse $BACKUP_BRANCH)
-LAST_TREE=$(git rev-parse $BACKUP_BRANCH^{tree})
+LAST_COMMIT=$(git rev-parse --revs-only $BACKUP_BRANCH)
+LAST_TREE=$(git rev-parse --revs-only $BACKUP_BRANCH^{tree})
 
-git add --all
+echo 'Create index'
+git add --verbose --all
+echo 'Write index'
 NEXT_TREE=$(git write-tree)
 
 echo lc:$LAST_COMMIT lt:$LAST_TREE nt:$NEXT_TREE
 
-#rm -f $GIT_INDEX_FILE
+if [ $LAST_TREE!=$NEXT_TREE ]
+then
+    if [ $LC!= ]
+    then
+        echo Write first commit
+        CSHA=$(echo $BACKUP_MESSAGE | git commit-tree $NEXT_TREE)
+    else
+        echo Write commit
+        CSHA=$(echo $BACKUP_MESSAGE | git commit-tree -p $LAST_COMMIT $NEXT_TREE)
+    fi
+    echo Update branch $BACKUP_BRANCH
+    git update-ref $BACKUP_BRANCH $CSHA
+fi
+
+rm -rf $TMPDIR
